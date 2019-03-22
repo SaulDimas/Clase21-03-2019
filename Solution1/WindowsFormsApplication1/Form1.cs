cdcc4b0d4d7b23e331178bc51cd980285d1c9216
SaulDimas <48740297+SaulDimas@users.noreply.github.com>
2019-03-21 21:48:57 -0600

    Clase 21-03-2019
    Clase 21-03-2019

---
+++ Solution1/WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+	public partial class frmPrincipal : Form
+	{
+		public frmPrincipal()
+		{
+			InitializeComponent();
+			textBox1.Text = "0";
+			textBox2.Text = "0";
+		}
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+
+		}
+
+		private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+		{
+
+		}
+
+		private void label3_Click(object sender, EventArgs e)
+		{
+
+		}
+
+		private void button1_Click_1(object sender, EventArgs e)
+		{
+			int a, b, r;
+			a = int.Parse(textBox1.Text);
+			b = int.Parse(textBox2.Text);
+			r = a + b;
+			label3.Text = r.ToString();
+		}
+
+		private void button2_Click(object sender, EventArgs e)
+		{
+			int a, b, r;
+			a = int.Parse(textBox1.Text);
+			b = int.Parse(textBox2.Text);
+			r = a - b;
+			label3.Text = r.ToString();
+		}
+
+		private void button3_Click(object sender, EventArgs e)
+		{
+			int a, b, r;
+			a = int.Parse(textBox1.Text);
+			b = int.Parse(textBox2.Text);
+			r = a * b;
+			label3.Text = r.ToString();
+		}
+
+		private void button4_Click(object sender, EventArgs e)
+		{
+			int a, b, r;
+			String Datos;
+			a = int.Parse(textBox1.Text);
+			b = int.Parse(textBox2.Text);
+			r = a / b;
+			Datos = "Resultado " + r.ToString();
+			label3.Text = r.ToString();
+		}
+
+		private void label3_Click_1(object sender, EventArgs e)
+		{
+
+		}
+	}
+}
